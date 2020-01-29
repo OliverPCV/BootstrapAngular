1f8477bcbbdbd36138a585789923cc3c1dcccadd
OliverPCV <44001532+OliverPCV@users.noreply.github.com>
2020-01-29 21:49:43 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {GalerryComponent} from './galerry/galerry.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'aboutus', component: AboutusComponent},
+  {path: 'galerry', component: GalerryComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
