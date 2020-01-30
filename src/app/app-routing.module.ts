de760a0ad0efad2925d01c1752f8a9859fca4254
OliverPCV <44001532+OliverPCV@users.noreply.github.com>
2020-01-30 07:57:37 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'aboutus', component: AboutusComponent},
   {path: 'galerry', component: GalerryComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
 ];
 
 @NgModule({
